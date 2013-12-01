a190bbf797775552ebb52409d476073b78e45c5d
denizozger <deniz@parallelbrains.com>
2013-12-01 21:31:35 +0000

    Passport.JS Google auth, securing /user path, fault tolerence for Redis

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const
   express = require('express'),
   app = express(),
   redisClient = require('redis').createClient(),
-  RedisStore = require('connect-redis')(express);
+  RedisStore = require('connect-redis')(express),
+  passport = require('passport'),
+  GoogleStrategy = require('passport-google').Strategy,
+  log = require('npmlog');
 
 app.use(express.logger('dev')),
 app.use(express.cookieParser());
@@ -14,14 +17,75 @@ app.use(express.session({
     client: redisClient
   })
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static(__dirname + '/static'));
 app.use(express.static(__dirname + '/bower_components'));
+
+redisClient
+.on('ready', function() { log.info('REDIS', 'ready'); })
+.on('error', function(err) { log.error('REDIS', err.message); });
+
+/**
+ * User authentication
+ */
+passport.serializeUser(function(user, done) {
+  done(null, user.identifier);
+});
+
+passport.deserializeUser(function(id, done) {
+  done(null, { identifier: id });
+});
+
+passport.use(new GoogleStrategy({
+    returnURL: 'http://localhost:3000/auth/google/return',
+    realm: 'http://localhost:3000/'
+  },
+  function(identifier, profile, done) {
+    profile.identifier = identifier;
+    return done(null, profile);
+  }
+));
+
+// Authentication middleware
+const authed = function(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else if (redisClient.ready) {
+    res.json(403, {
+      error: "forbidden",
+      reason: "not_authenticated"
+    });
+  } else {
+    res.json(503, {
+      error: "service_unavailable",
+      reason: "authentication_unavailable"
+    });
+  }
+};
+
+/**
+ * Public API
+ */
 
 app.get('/api/:name', function(req, res) {
   res.json(200, { "hello": req.params.name });
 });
 
+app.get('/api/user', authed, function(req, res){
+  res.json(req.user);
+});
+
 app.listen(3000, function(){
   console.log("ready captain.");
+});
+
+app.get('/auth/google/:return?',
+  passport.authenticate('google', { successRedirect: '/' })
+);
+
+app.get('/auth/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
 });
