50c788abda62a6059288c32609d62fee517b9a00
denizozger <deniz@parallelbrains.com>
2014-02-22 20:07:59 +0000

    retrieving list of things and logged in user from the session, saving more details to DB per record

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const
   GoogleStrategy = require('passport-google').Strategy,
   request = require('request'),
   log = require('npmlog'),
-  http = require('http');
+  http = require('http'),
+  async = require('async');
 
 app.use(express.logger('dev')),
 app.use(express.cookieParser());
@@ -37,7 +38,7 @@ const config = {
  * User authentication
  */
 passport.serializeUser(function(user, done) {
-  done(null, user.identifier);
+  done(null, user);
 });
 
 passport.deserializeUser(function(id, done) {
@@ -50,6 +51,7 @@ passport.use(new GoogleStrategy({
   },
   function(identifier, profile, done) {
     profile.identifier = identifier;
+    // todo? here we can save the user to DB
     return done(null, profile);
   }
 ));
@@ -57,6 +59,7 @@ passport.use(new GoogleStrategy({
 // Authentication middleware
 const authed = function(req, res, next) {
   if (req.isAuthenticated()) {
+    console.log(req.user)
     return next();
   } else if (redisClient.ready) {
     res.json(403, {
@@ -81,20 +84,32 @@ app.listen(3000, function(){
 
 app.post('/api/threethings', [authed, express.json()], function(req, res) {
 
+  var today = new Date();
+
   var things = {
     first : req.body.firstthing,
     second : req.body.secondthing,
-    third : req.body.thirdthing
+    third : req.body.thirdthing,
+    addedBy : {
+      name : req.user.identifier.name.givenName,
+      surname : req.user.identifier.name.familyName
+    },
+    addedDate : {
+      day : today.getDate(),
+      month : today.getMonth(),
+      year : today.getFullYear()
+    }
   };
 
-  var userId = encodeURIComponent(req.user.identifier);
-  var today = new Date();
-
-  var thingsURL = config.thingsdb + '1/' + 
-    today.getDate() + today.getMonth() + today.getFullYear();
+  var userId = encodeURIComponent(req.user.identifier.identifier);
   
-  // console.log(thingsURL);
-  // console.log(things);
+
+  var thingsURL = config.thingsdb + '1-' + 
+    // today.getDate() + today.getMonth() + today.getFullYear();
+    getRandomInt(1, 1000);
+  
+  log.info('Adding record to DB. URL: ' + thingsURL + ' , values: ' + 
+    things.first + ' ' + things.second + ' '  + things.third);
 
   request({
     method: 'PUT',
@@ -105,30 +120,61 @@ app.post('/api/threethings', [authed, express.json()], function(req, res) {
       //throw Error(err);
       log.error(err);
     }
-    //log.verbose(res.statusCode, things);
+    log.verbose(res.statusCode, things);
   });
 
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end();
 });
 
-app.get('/api/threethings', authed, function(req, res){
+app.get('/api/threethings', authed, function(req, res) {
 
-  function getThings(callback) {
+  function getAllThings(callback) {
     request({
       method: 'GET',
-      url: 'http://localhost:5984/things/1/2302014'
+      url: 'http://localhost:5984/things/_all_docs'
     }, function(err, res, body) {
       callback(JSON.parse(body));
     });
   }
 
-  function handleThings(things){
-    res.json([things]);
+  function handleAllThings(allThings) {
+
+    var allThingsData = [];
+    var completedCalls = 0;
+
+    async.each(allThings.rows, function(thing, callback) {
+      log.silly('Getting thing ' + thing.id);
+
+      function addThingData(thingData) {
+        completedCalls++;
+        allThingsData.push(thingData);
+
+        if (completedCalls === allThings.rows.length) {
+          log.info('Retrieved ' + allThingsData.length + ' records');
+          res.json(allThingsData);
+        }
+      }
+
+      request({
+        method: 'GET',
+        url: 'http://localhost:5984/things/' + thing.id
+      }, function(err, res, body) {
+        log.silly('Retrieved from DB: ' + JSON.parse(body).id);
+        addThingData(JSON.parse(body));
+      });
+
+      callback();
+    }, function(err){
+      if (err) {
+        log.error('Error: ' + err)
+      }   
+    });
+      
   }
 
-  getThings(function(things){
-    handleThings(things);        
+  getAllThings(function(things){
+    handleAllThings(things);        
   });
 });
 
@@ -147,3 +193,16 @@ app.get('/auth/logout', function(req, res){
   req.logout();
   res.redirect('/');
 });
+
+app.get('/api/session', function(req, res) {
+    if (req.session) {
+        res.json(req.session);
+    } else {
+        res.send('You have no session.');
+    }
+});
+
+// Temporary
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
